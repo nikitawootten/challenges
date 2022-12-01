473fbf89f3a2f656fd343dce2dac3098f4b673e5
Nikita Wootten <nikita.wootten@gmail.com>
2022-12-01 11:51:37 -0500

    Played around a bit with indentation

--- advent-of-code/2022/day01/day01.hs
+++ advent-of-code/2022/day01/day01.hs
@@ -30,10 +30,9 @@ sortDesc = sortOn Down
 day2 x = sumList $ take 3 $ sortDesc x
 
 main :: IO ()
-main = do
-  withFile "input.txt" ReadMode $ \handle -> do
-    contents <- hGetContents handle
-    let itemsPerElf = cleanInput contents
-    let totalCaloriesPerElf = map sumList itemsPerElf
-    printf "Day 1: %v\n" (day1 totalCaloriesPerElf)
-    printf "Day 2: %v\n" (day2 totalCaloriesPerElf)
+main = withFile "input.txt" ReadMode $ \handle -> do
+  contents <- hGetContents handle
+  let itemsPerElf = cleanInput contents
+  let totalCaloriesPerElf = map sumList itemsPerElf
+  printf "Day 1: %v\n" (day1 totalCaloriesPerElf)
+  printf "Day 2: %v\n" (day2 totalCaloriesPerElf)
